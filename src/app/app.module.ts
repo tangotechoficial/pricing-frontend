cd375a1ca8a3f7be8887ba9152fa7efcd7cd73f9
ivanlynch <ivan.lynch@osh-pro.com>
2020-01-14 13:49:47 -0300

    Configuracion de ruteo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {routing, appRoutingProviders} from './app.routing';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, appRoutingProviders]
 })
 export class AppModule { }
