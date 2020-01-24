0f8812f65c9fe29032c560999695f87802368f27
Iván Lynch <ivan.lynch@outlook.com.ar>
2020-01-24 12:32:23 -0300

    Agregue el modelo de sacceso y armando la logica de la vista

---
+++ src/app/models/sacceso.ts
@@ -0,0 +1,9 @@
+import { element } from 'protractor';
+
+export class Sacceso {
+    constructor(
+        public sId: string,
+        public sSeqAcceso: string,
+        public sDesAcceso: string
+    ){}
+}
