3d9a315dbcd7f39c77e2cf250666262de50f0efc
ivanlynch <ivan.lynch@osh-pro.com>
2020-02-07 13:06:15 -0300

    Se agrego el mensaje de bienvenida cuando el login es exitoso

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-menu',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
-
+  public sCurrentUser = JSON.parse(localStorage.getItem("User"));
   constructor() { }
-
+  
   ngOnInit() {
+    console.log(this.sCurrentUser);
+    setTimeout(function(){
+      $('.alert').hide();
+    },
+    2000);
   }
 
 }
