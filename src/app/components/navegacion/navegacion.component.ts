59f406a63138cd0e0e83ad8518e5951c20ab46fe
Charlie Minnelli <cminnelli@gmail.com>
2020-02-25 01:06:49 -0300

    interaccion notif

--- src/app/components/navegacion/navegacion.component.ts
+++ src/app/components/navegacion/navegacion.component.ts
@@ -1,11 +1,49 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { Location } from "@angular/common";
+import {BrowserModule} from '@angular/platform-browser'
+
 declare var $: any;
+
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  query,
+  animateChild
+} from '@angular/animations';
 
 @Component({
   selector: 'navegacion',
   templateUrl: './navegacion.component.html',
+  animations: [
+  trigger('ngIfAnimation', [
+    transition(':enter, :leave', [
+      query('@*', animateChild())
+    ])
+  ]),
+
+  trigger('easeInOut', [
+    transition('void => *', [
+        style({
+            opacity: 0,
+        }),
+        animate("500ms ease-in", style({
+            opacity: 1,
+        }))
+    ]),
+    transition('* => void', [
+        style({
+            opacity: 1
+        }),
+        animate("500ms ease-in", style({
+            opacity: 0
+        }))
+      ])
+    ])
+],
   styleUrls: ['./navegacion.component.css' , './navegacion.component.scss']
 })
 
@@ -15,6 +53,8 @@ export class NavegacionComponent implements OnInit {
   public currentUrl: string;
   public numNotif:number = 3 ;
   public numAprob:number = 1;
+
+  public showDetail:boolean = false;
 
   private mapUrlToSection = {
     "/menu": "Inicio",
@@ -46,9 +86,8 @@ export class NavegacionComponent implements OnInit {
   notifyOpen(){
       $("#notificationPopUp").fadeToggle(400);
   }
-  toggleDetail(target){
-     var target = event.target || event.srcElement || event.currentTarget;
-     console.log(target)
+  toggleDetail(){
+    this.showDetail = !this.showDetail;
   }
 
 
