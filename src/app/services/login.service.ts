7b9f32d5ed9fe748878f5cf0a970504f63875e9c
Isaque Alves <isaquealves@gmail.com>
2020-03-06 21:12:14 -0300

    Ajusta login.service para usar a variavel apiUrl

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,21 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 import { User } from '../models/user';
 import { Global } from './global';
+import { environment} from '@env/environment';
 
-@Injectable()
+@Injectable({providedIn: 'root'})
 export class LoginService{
     public url: string;
     constructor(
         private _http: HttpClient
     ){
-        this.url = Global.url;
+        this.url = `${environment.apiUrl}/login`;
     }
 
     login(user: User): Observable <any>{
         let params = JSON.stringify(user);
         let headers = new HttpHeaders().set('Content-type', 'application/json');
-        return this._http.post(this.url + '/login', params, {headers: headers});
+        return this._http.post(this.url, params, {headers: headers});
     }
 }
