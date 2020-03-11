e201a3fd4fdafcec0a7e7ade7bb864e51f5966d0
Iván Lynch <ivan.lynch@outlook.com.ar>
2020-03-11 18:39:09 -0300

    Lint rules applied to SACCESO Componente

--- src/app/components/sacceso/sacceso.component.ts
+++ src/app/components/sacceso/sacceso.component.ts
@@ -2,15 +2,16 @@ import { Component, OnInit } from '@angular/core';
 import { Sacceso } from '../../models/sacceso';
 import { MetadataService } from './../../services/metadata.service';
 import { SaccesoService } from '../../services/sacceso.service';
-import { NgxSpinnerService } from "ngx-spinner";
+import { NgxSpinnerService } from 'ngx-spinner';
 
 declare var $: any;
 
 @Component({
+  // tslint:disable-next-line: component-selector
   selector: 'sacceso',
   templateUrl: './sacceso.component.html',
   styleUrls: ['./sacceso.component.scss'],
-  providers: [MetadataService, SaccesoService, NgxSpinnerService]
+  providers: [SaccesoService, NgxSpinnerService]
 })
 
 export class SaccesoComponent implements OnInit {
@@ -19,30 +20,29 @@ export class SaccesoComponent implements OnInit {
   searchSeleccionado;
   selectedValue: string;
   public existSelected = false;
-  public errDesc = "";
-  public _sacceso: Sacceso;
-  public _saMessage: Sacceso;
-  public _newSA: Sacceso;
-  public _nextVal: string;
+  public errDesc = '';
+  public sacceso: Sacceso;
+  public saMessage: Sacceso;
+  public newSA: Sacceso;
+  public nextVal: string;
   public sequenciasAcceso: Array<any>;
   public sequenciasAccesoSearch: Array<any>;
   public selectedProperties: Array<any>;
   public searchValues: Array<any>;
   public selValues1: Array<any>;
   public selValues2: Array<any>;
-  public saveSuccess: boolean = false;
-  public saveError: boolean = false;
+  public saveSuccess = false;
+  public saveError = false;
   public inputDescripcion: string;
 
 
   constructor(
-    private _metadataService: MetadataService,
-    private _saccesoService: SaccesoService,
+    private saccesoService: SaccesoService,
     private spinner: NgxSpinnerService
   ) { }
 
   public closePopUp() {
-    $('#myModal').modal('hide')
+    $('#myModal').modal('hide');
   }
 
   public openPop() {
@@ -57,20 +57,20 @@ export class SaccesoComponent implements OnInit {
     this.searchValues = new Array<any>();
     this.selValues1 = new Array<any>();
     this.selValues2 = new Array<any>();
-    this._sacceso = new Sacceso();
-    this._newSA = new Sacceso();
-    this._saMessage = new Sacceso();
+    this.sacceso = new Sacceso();
+    this.newSA = new Sacceso();
+    this.saMessage = new Sacceso();
 
-    //Update SeqCampo elements
-    this.updateSeqCampo()
+    // Update SeqCampo elements
+    this.updateSeqCampo();
 
-    //Updates the _newSA with the last Cod_Campo
-    this.getLastSeqCampo()
+    // Updates the newSA with the last Cod_Campo
+    this.getLastSeqCampo();
 
-    //Updates the _sacceso with the last Cod_Sequencia
-    this.getLastSequencia()
+    // Updates the sacceso with the last Cod_Sequencia
+    this.getLastSequencia();
 
-    $('div[contenteditable]').keydown(function (e) {
+    $('div[contenteditable]').keydown((e: any) => {
       if (e.keyCode === 13) {
         document.execCommand('insertHTML', false, '<br>');
         return false;
@@ -79,17 +79,17 @@ export class SaccesoComponent implements OnInit {
   }
 
 
-  /* Iván Lynch - 06/03/2020 
-     Input: null 
+  /* Iván Lynch - 06/03/2020
+     Input: null
      Output: null
      This function updates the SeqCampo elements
   */
   public updateSeqCampo() {
-    //Get values from SeqCampo
+    // Get values from SeqCampo
     this.sequenciasAcceso = new Array<any>();
-    this._saccesoService.getSaccesoList().subscribe((values) => {
+    this.saccesoService.getSaccesoList().subscribe((values) => {
       values.map((elem) => {
-        let seq = new Sacceso();
+        const seq = new Sacceso();
         seq.setId(elem.id);
         seq.setCodigo(elem.Cod_Campo);
         seq.setDescription(elem.Nome_Campo);
@@ -99,235 +99,196 @@ export class SaccesoComponent implements OnInit {
     });
   }
 
-  /* Iván Lynch - 06/03/2020 
-     Input: null 
+  /* Iván Lynch - 06/03/2020
+     Input: null
      Output: null
-     This function updates the _newSA Object
+     This function updates the newSA Object
   */
   public getLastSeqCampo() {
-    this._saccesoService.getLastSeqCampo().subscribe((value) => {
-      var parsedValue;
-      parsedValue = value;
-      var oldCode = parsedValue.Cod_Campo;
-      this._newSA.setCodigo(this.evaluateNextSA(oldCode));
+    this.saccesoService.getLastSeqCampo().subscribe((value: any) => {
+      const oldCode = value.Cod_Campo;
+      this.newSA.setCodigo(this.evaluateNextSA(oldCode));
     });
   }
 
-  /* Iván Lynch - 06/03/2020 
-     Input: null 
+  /* Iván Lynch - 06/03/2020
+     Input: null
      Output: null
-     This function updates the _sacceso Object
+     This function updates the sacceso Object
   */
   public getLastSequencia() {
     this.spinner.show();
-    this._saccesoService.getLastSequencia()
+    this.saccesoService.getLastSequencia()
       .then((result: any) => {
-        this._sacceso.setCodigo(this.evaluateNextSA(result.Cod_Sequencia))
-        this.spinner.hide()
-      })
+        this.sacceso.setCodigo(this.evaluateNextSA(result.Cod_Sequencia));
+        this.spinner.hide();
+      });
   }
 
-  /* Iván Lynch - 05/03/2020 
-     Input: String Code 
-     Output: String Code + 1 
-     ie: 
+  /* Iván Lynch - 05/03/2020
+     Input: String Code
+     Output: String Code + 1
+     ie:
         Input: CP001
-        Output: CP002 
+        Output: CP002
   */
-  public evaluateNextSA(code: String) {
-    let codeString = code.substr(0, 2);
-    let codeNumer = parseInt(code.substr(code.length - 2, code.length));
-    let nextValue = codeNumer + 1;
-    let nextCode = codeString + this.pad_with_zeroes(nextValue, 3);
+  public evaluateNextSA(code: any) {
+    const codeString = code.substr(0, 2);
+    // tslint:disable-next-line: radix
+    const codeNumer = parseInt(code.substr(code.length - 2, code.length));
+    const nextValue = codeNumer + 1;
+    const nextCode = codeString + this.pad_with_zeroes(nextValue, 3);
     return nextCode;
   }
 
   public onSelectedValue(event) {
     this.selectedValue = event;
-    this._sacceso = event;
+    this.sacceso = event;
     this.existSelected = true;
-    setTimeout(function () {
+    setTimeout(function() {
       this.existSelected = false;
-    }.bind(this), 3000)
+    }.bind(this), 3000);
   }
 
   onDltSelection(val) {
-    var selectedIndex = '';
+    let selectedIndex = '';
     this.selectedProperties.forEach((elem, index) => {
-      if (elem.getCodigo() == val.getCodigo()) {
+      if (elem.getCodigo() === val.getCodigo()) {
         this.sequenciasAcceso.forEach((sElem, index2) => {
-          if (sElem.getCodigo() == val.getCodigo()) {
+          if (sElem.getCodigo() === val.getCodigo()) {
             selectedIndex = index2.toString();
           }
-        })
+        });
         this.selectedProperties.splice(index, 1);
-        this._sacceso._parents.slice(index, 1);
+        this.sacceso._parents.slice(index, 1);
       }
-    })
+    });
 
-    const elements = document.getElementsByClassName("custom-control-input");
-    const aElems = Array.prototype.slice.call(elements)
-    for (var i = 0; i < aElems.length; i++) {
-      const elemLabels = aElems[i].labels
-
-      if (elemLabels[0].innerText === val.getDescription()) {
-        aElems[i].click();
+    const elements = document.getElementsByClassName('custom-control-input');
+    const aElems = Array.prototype.slice.call(elements);
+    aElems.forEach((elem: any, index: any) => {
+      const elemLabel = elem.labels;
+      if (elemLabel[0].innerText === val.getDescription()) {
+        aElems[index].click();
       }
-    }
+    });
   }
 
   public checkValue(sa: Sacceso) {
-    //Iterate over SEQ_CAMPOS
+    // Iterate over SEQ_CAMPOS
     this.sequenciasAcceso.map(elem => {
-      
-      //If selected elemement sa is equal to current element in the array loop
-      if (elem.getId() == sa.getId()) {
-        console.log("Selected element is equal to current elem", elem, sa);
 
-        //Check if current element is already selected
+      // If selected elemement sa is equal to current element in the array loop
+      if (elem.getId() === sa.getId()) {
+
+        // Check if current element is already selected
         if (elem.isSelected()) {
 
-          //Unselect current element
+          // Unselect current element
           elem.setSelected(false);
-          console.log("Current selected item", elem);
 
-          //Iterate over selected properties to remove current selected item
+          // Iterate over selected properties to remove current selected item
           this.selectedProperties.map((elem2, index) => {
 
-            //If current selected item exist on the the array
-            if (elem2.getId() == elem.getId()) {
-              console.log("SelectedProperties array items :", elem2, elem)
-
-              //Remove elem from array
+            // If current selected item exist on the the array
+            if (elem2.getId() === elem.getId()) {
+              // Remove elem from array
               this.selectedProperties.splice(index, 1);
-              this._sacceso._parents = this._sacceso._parents.filter((obj) => {
-                return obj.getCodigo() !== sa.getCodigo()
-              })
+              this.sacceso._parents = this.sacceso._parents.filter((obj) => {
+                return obj.getCodigo() !== sa.getCodigo();
+              });
             }
-          })
+          });
         } else {
-          this._sacceso._parents.push(sa);
-          console.log(this._sacceso._parents);
+          this.sacceso._parents.push(sa);
+          console.log(this.sacceso._parents);
           elem.setSelected(true);
           this.selectedProperties.push(elem);
         }
       }
-    })
+    });
 
-    var descripcion = '';
+    let descripcion = '';
     this.selectedProperties.forEach((elem, index) => {
-      if (index == 0) {
+      if (index === 0) {
         descripcion = descripcion + elem.getDescription();
       } else {
         descripcion = descripcion + '/' + elem.getDescription();
       }
-    })
-    this._sacceso.sDesAcceso = descripcion;
+    });
+    this.sacceso.sDesAcceso = descripcion;
   }
 
   submitNewSA() {
-    this._saccesoService.postSacceso(this._newSA)
+    this.saccesoService.postSacceso(this.newSA)
       .subscribe(response => {
         this.saveSuccess = true;
         this.updateSeqCampo();
       },
         error => {
-          console.log(error.error.Nome_Campo[0] == "se q_campo with this Nome Campo already exists.");
-          this.errDesc = "O campo já existe"
+          console.log(error.error.Nome_Campo[0] === 'se q_campo with this Nome Campo already exists.');
+          this.errDesc = 'O campo já existe';
           this.saveError = true;
-          setTimeout(function () {
+          setTimeout(function() {
             this.saveError = false;
-          }.bind(this), 2000)
-        })
+          }.bind(this), 2000);
+        });
 
-    this._saMessage.setCodigo(this._newSA.getCodigo());
-    this._saMessage.setDescription(this._newSA.getDescription());
+    this.saMessage.setCodigo(this.newSA.getCodigo());
+    this.saMessage.setDescription(this.newSA.getDescription());
 
-    setTimeout(function () {
+    setTimeout(function() {
       this.saveSuccess = false;
       this.getLastSeqCampo();
-    }.bind(this), 2000)
-    this._newSA = new Sacceso();
+    }.bind(this), 2000);
+    this.newSA = new Sacceso();
 
   }
 
-  public pad_with_zeroes(number, length) {
-
-    var my_string = '' + number;
-    while (my_string.length < length) {
-      my_string = '0' + my_string;
+  public pad_with_zeroes(num, length) {
+    let myString = '' + num;
+    while (myString.length < length) {
+      myString = '0' + myString;
     }
-
-    return my_string;
-
+    return myString;
   }
 
   public submitSA() {
-    console.log(this._sacceso)
     this.spinner.show();
-    if (this._sacceso._parents.length < 2) {
-      this.errDesc = "Selecione mais de um campo"
-      this.saveError = true;
-      this.spinner.hide()
-      setTimeout(function () {
-        this.saveError = false;
-      }.bind(this), 2000)
-
-    } else {
-      this._saccesoService.postSaccesoComp(this._sacceso)
-        .then(data => {
-          this.saveSuccess = true;
-          $('#myModal').modal('show');
-          this.spinner.hide()
-          this.getLastSequencia();
-          setTimeout(function () {
-            this.saveSuccess = false;
-          }.bind(this), 2000)
-          this._saMessage.setCodigo(this._sacceso.getCodigo());
-          this._saMessage.setDescription(this._sacceso.getDescription());
-          this._sacceso = new Sacceso();
-          this.selectedProperties.map(elem => {
-            this.onDltSelection(elem);
-          })
-          this.selectedProperties.forEach(elem => {
-            this.onDltSelection(elem);
-          })
-          this.selectedProperties.map(elem => {
-            this.onDltSelection(elem);
-          })
-          this.selectedProperties.map(elem => {
-            this.onDltSelection(elem);
-          })
-        })
-        .catch((err) => {
-          console.log(err)
-          if (err.error.Nome_Sequencia[0] == "sequencia with this Nome Sequencia already exists.") {
-            this.errDesc = "A sequência de acesso já existe."
-            this.saveError = true;
-            this.spinner.hide()
-            setTimeout(function () {
-              this.saveError = false;
-            }.bind(this), 2000)
-          }
-        })
-    }
-
-    /* this._saccesoService.postSaccesoComp(this._sacceso)
-      .subscribe((response: any) => {
+    this.saccesoService.postSaccesoComp(this.sacceso)
+      .then(data => {
         this.saveSuccess = true;
         $('#myModal').modal('show');
+        this.spinner.hide();
         this.getLastSequencia();
-      },
-      error => {
-        console.log(error.error.Nome_Sequencia[0] == "sequencia with this Nome Sequencia already exists.");
-        this.errDesc = "A sequência de acesso já existe."
-        this.saveError = true;
-        setTimeout(function () {
-          this.saveError = false;
-        }.bind(this), 2000)
-      }) */
-
-
+        setTimeout(function() {
+          this.saveSuccess = false;
+        }.bind(this), 2000);
+        this.saMessage.setCodigo(this.sacceso.getCodigo());
+        this.saMessage.setDescription(this.sacceso.getDescription());
+        this.sacceso = new Sacceso();
+        this.selectedProperties.map(elem => {
+          this.onDltSelection(elem);
+        });
+        this.selectedProperties.forEach(elem => {
+          this.onDltSelection(elem);
+        });
+        this.selectedProperties.map(elem => {
+          this.onDltSelection(elem);
+        });
+        this.selectedProperties.map(elem => {
+          this.onDltSelection(elem);
+        });
+      })
+      .catch((err) => {
+        if (err.error.Nome_Sequencia[0] === 'sequencia with this Nome Sequencia already exists.') {
+          this.errDesc = 'A sequência de acesso já existe.';
+          this.saveError = true;
+          this.spinner.hide();
+          setTimeout(function() {
+            this.saveError = false;
+          }.bind(this), 2000);
+        }
+      });
   }
-
 }
