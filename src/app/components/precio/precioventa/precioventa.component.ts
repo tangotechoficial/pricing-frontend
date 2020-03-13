f746ca2b2e3961094b9b50c7e446c8850cd7634c
Andres <and.atencio@gmail.com>
2020-03-13 00:28:03 -0300

    precioventas

--- src/app/components/precio/precioventa/precioventa.component.ts
+++ src/app/components/precio/precioventa/precioventa.component.ts
@@ -1,22 +1,100 @@
-import { Component, OnInit } from '@angular/core';
+
+import { Component, OnInit } from "@angular/core";
+import { CondicionService } from "../../../services/condicion.service"
 
 @Component({
-  selector: 'precioventa',
-  templateUrl: './precioventa.component.html',
-  styleUrls: ['../../precio/precio.component.scss']
+  selector: "precioventa",
+  templateUrl: "./precioventa.component.html",
+  styleUrls: ["../../precio/precio.component.scss"],
+  providers: [CondicionService]
 })
 export class PrecioVentaComponent implements OnInit {
   public sCurrentUser = JSON.parse(localStorage.getItem("User"));
   public bBusiness: boolean;
 
-  constructor() { }
+  camadas = [];
+  isShow: boolean;
+  existNegocios: any;
+  existVentas: any;
+  loading: boolean = true;
+  
+  constructor(
+    private _condicionService: CondicionService
+  ) {}
 
   ngOnInit() {
-    if(this.sCurrentUser.type !== "technical"){
-      this.bBusiness = true;
-    }else{
-      this.bBusiness = false;
-    }
-  }
+    // traer por servicio cada condicion
+    this._condicionService.getCamadas()
+    .then(data => {
+      this.camadas = this.parseResponseCamada(data)
+      this.loading = false
+    })
+    .catch(err => alert(err))
+
+    
+    const camadas =  this._condicionService.getCamadas()
+    const camadaEsquemas = this._condicionService.getCamadaEsquema();
+    const condicaoCamadas = this._condicionService.getCondicaoCamada();
+    const condicaos = this._condicionService.getCondicaoByCode();
+    const promisesFetch = [camadas, camadaEsquemas, condicaoCamadas, condicaos];
+
+    Promise.all(promisesFetch).then(
+      ([camadas, camadaEsquemas, condicaoCamadas, condicaos]) => {
+        camadas = camadas.filter(e => e.TIPO_BASE_VENDAS === "V")
+        const camadasFullData = camadas.map(camada => {
+          let condicaoCamadasFilter = camadaEsquemas.map(camadaEsquema => {
+            return condicaoCamadas.filter(
+              condCamada => condCamada.id == camadaEsquema.id_Condicao_Camada
+            )[0];
+          });
   
+          let condicaosFilter = condicaoCamadasFilter.map(condCamada => {
+            return condicaos.filter(cond => cond.id == condCamada.id_Condicao)[0];
+          });
+  
+          let condicaosByCamadaFilter = condicaosFilter.filter(cond => cond.id_Camada == camada.id)
+  
+          let condicaosAllow = condicaoCamadas.filter(condCam => condCam.id_Camada == camada.id).map(condCamada => {
+            return condicaos.filter(cond => cond.id == condCamada.id_Condicao)[0];
+          })
+  
+          
+          return {
+            camada,
+            condicaosAllow,
+            condicaos: condicaosByCamadaFilter
+          }
+        })
+
+        this.camadas = camadasFullData
+        // Mock data CAMADA_ESQUEMA
+        // camadaEsquemas = [{
+        //   id: 1,
+        //   id_Condicao_Camada: 1,
+        //   id_Esquema: 1
+        // }]
+
+
+
+        
+      }
+    );
+  }
+
+  parseResponseCamada(data) {
+    return data.filter(e => e.TIPO_BASE_VENDAS === "B")
+  }
+
+  goToSection() {
+    //this._router.navigate(['/pbase']);
+    this.isShow = !this.isShow;
+  }
+
+  parentListener($event) {
+    this.existNegocios = $event;
+  }
+
+  parentListenerTwo($event) {
+    this.existVentas = $event;
+  }
 }
