95048d1cccd37530ceed02736592497e67f202eb
Iván Lynch <ivan.lynch@outlook.com.ar>
2020-03-13 10:47:05 -0300

    Fixed Condicao

--- src/app/services/condicion.service.ts
+++ src/app/services/condicion.service.ts
@@ -3,7 +3,6 @@ import { HttpClient} from '@angular/common/http';
 import { Global } from './global';
 import { Condicion } from '../models/condicion';
 import { map } from 'rxjs/operators';
-import { ModelCondicao } from 'app/models/condicion.model';
 
 @Injectable()
 export class CondicionService {
@@ -19,18 +18,23 @@ export class CondicionService {
     getCamadas(): Promise<any> {
         return this.http.get(this.url + '/camada/', {headers: {'Content-type': 'application/json'}}).toPromise();
     }
+
     getChaveContas(): Promise<any> {
         return this.http.get(this.url + '/chavecontas/', {headers: {'Content-type': 'application/json'}}).toPromise();
     }
+
     getTiposValor(): Promise<any> {
         return this.http.get(this.url + '/tipovalor/', {headers: {'Content-type': 'application/json'}}).toPromise();
     }
+
     getSequenciasAcesso(): Promise<any> {
         return this.http.get(this.url + '/sequencia/', {headers: {'Content-type': 'application/json'}}).toPromise();
     }
+
     getSequenciasByCondicao(): Promise<any> {
         return this.http.get(this.url + '/condicaosequencia/', {headers: {'Content-type': 'application/json'}}).toPromise();
     }
+
     postCondicao(co: Condicion): Promise<any> {
         return this.http.post(this.url + '/condicao/', {
             Cod_Condicao: co.sCodCondicion,
@@ -50,6 +54,53 @@ export class CondicionService {
                     this.postCondicaoSequencia(elem.id, campo.id).subscribe();
                 }))
          ).toPromise();
+    }
+
+    editCondicao(co: Condicion): Promise<any> {
+        return this.http.put(this.url + '/condicao/' + co.sId + '/', {
+            Cod_Condicao: co.sCodCondicion,
+            Desc_Condicao: co.sDesCondicion,
+            Escala_Qtde: co.bEscalaQtde ? 1 : 0,
+            POS_NEG: co.bNeg ? 'N' : 'P',
+            TIP_BASE_VENDAS: co.oCamada.TIPO_BASE_VENDAS,
+            MANDATORIA: 0,
+            ESTATISTICA: 0,
+            id_Camada: co.oCamada.id,
+            id_ChaveContas: co.oChaveContas.id,
+            id_TipoValor: co.oTipoValor.id
+        }, {headers: {'Content-type': 'application/json'}})
+         .pipe(
+             map((elem: any) =>
+                co.aSequencias.forEach(campo => {
+                    this.postCondicaoSequencia(elem.id, campo.id).subscribe();
+                }))
+         ).toPromise();
+    }
+
+    async deleteRelation(id: any) {
+        await this.http.delete(this.url + '/condicaosequencia/' + id + '/', {headers: {'Content-type': 'application/json'}}).toPromise();
+    }
+
+    getLastCondicao(): Promise<any> {
+        return this.http.get(this.url + '/condicao/last/', { headers: { 'Content-type': 'application/json' }}).toPromise();
+    }
+
+    deleteCondicaoSequencia(id: any) {
+        const arr: any = [];
+        const getSequenciasCondicao = this.http.get(
+            this.url + '/condicaosequencia/', {headers: {'Content-type': 'application/json'}}).toPromise();
+        getSequenciasCondicao.then(
+            (elems: any) => {
+                elems.map(
+                    (sc: any) => {
+                        if (sc.id_Condicao === id) {
+                            arr.push(sc);
+                        }
+                    });
+                arr.map((sc: any) => {
+                    this.deleteRelation(sc.id);
+                });
+            });
     }
 
     postCondicaoSequencia(condId, seqId) {
