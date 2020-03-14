bc68569582d1bedc909cd4a692ee5088eb7b45be
Andres <and.atencio@gmail.com>
2020-03-14 11:09:32 -0300

    refactor logic relation CAMADA_ESQUEMA CONDICAO_CAMADA CONDICAO CAMADA

--- src/app/components/precio/preciobase/preciobase.component.ts
+++ src/app/components/precio/preciobase/preciobase.component.ts
@@ -1,16 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-import { CondicionService } from '../../../services/condicion.service';
-import { NgxSpinnerService } from 'ngx-spinner';
+import { Component, OnInit } from "@angular/core";
+import { CondicionService } from "../../../services/condicion.service";
+import { NgxSpinnerService } from "ngx-spinner";
 
 @Component({
   // tslint:disable-next-line: component-selector
-  selector: 'preciobase',
-  templateUrl: './preciobase.component.html',
-  styleUrls: ['../../precio/precio.component.scss'],
+  selector: "preciobase",
+  templateUrl: "./preciobase.component.html",
+  styleUrls: ["../../precio/precio.component.scss"],
   providers: [CondicionService, NgxSpinnerService]
 })
 export class PrecioBaseComponent implements OnInit {
-  public sCurrentUser = JSON.parse(localStorage.getItem('User'));
+  public sCurrentUser = JSON.parse(localStorage.getItem("User"));
   public bBusiness: boolean;
 
   camadas = [];
@@ -23,10 +23,9 @@ export class PrecioBaseComponent implements OnInit {
   constructor(
     private condicionService: CondicionService,
     private spinner: NgxSpinnerService
-  ) { }
+  ) {}
 
   ngOnInit() {
-
     this.spinner.show();
 
     /*
@@ -34,50 +33,26 @@ export class PrecioBaseComponent implements OnInit {
       Gets type of user to validate
       which component will be shown
     */
-    if (this.sCurrentUser.type !== 'technical') {
+    if (this.sCurrentUser.type !== "technical") {
       this.bBusiness = true;
     } else {
       this.bBusiness = false;
     }
 
-    const camadas = this.condicionService.getCamadas();
-    const camadaEsquemas = this.condicionService.getCamadaEsquema();
-    const condicaoCamadas = this.condicionService.getCondicaoCamada();
-    const condicaos = this.condicionService.getCondicaoByCode();
-    const promisesFetch = [camadas, camadaEsquemas, condicaoCamadas, condicaos];
-
-    Promise.all(promisesFetch).then(
-      ([camadasRes, camadaEsquemasRes, condicaoCamadasRes, condicaosRes]) => {
-        camadasRes = camadasRes.filter((e: any) => e.TIPO_BASE_VENDAS === 'B');
-        const camadasFullData = camadasRes.map((camada: any) => {
-          const condicaoCamadasFilter = camadaEsquemasRes.map((camadaEsquema: any) => {
-            return condicaoCamadasRes.filter(
-              (condCamada: any) => condCamada.id === camadaEsquema.id_Condicao_Camada
-            )[0];
-          });
-          const condicaosFilter = condicaoCamadasFilter.map((condCamada: any) => {
-            return condicaosRes.filter(cond => cond.id === condCamada.id_Condicao)[0];
-          });
-          const condicaosByCamadaFilter = condicaosFilter.filter((cond: any) => cond.id_Camada === camada.id);
-          const condicaosAllow = condicaoCamadasRes.filter((condCam: any) => condCam.id_Camada === camada.id).map(condCamada => {
-            return condicaosRes.filter((cond: any) => cond.id === condCamada.id_Condicao)[0];
-          });
-          this.loading = false;
-          return {
-            camada,
-            condicaosAllow,
-            condicaos: condicaosByCamadaFilter
-          };
-        });
-
-        this.camadas = camadasFullData;
+    this.condicionService
+      .getCondicaoCamadaByCamadaEsquema("B")
+      .then(data => {
+        this.camadas = data;
         this.spinner.hide();
-      }
-    );
+      })
+      .catch(err => {
+        this.spinner.hide(); 
+        alert(err)
+      });
   }
 
   parseResponseCamada(data) {
-    return data.filter(e => e.TIPO_BASE_VENDAS === 'B');
+    return data.filter(e => e.TIPO_BASE_VENDAS === "B");
   }
 
   goToSection() {
--- src/app/services/condicion.service.ts
+++ src/app/services/condicion.service.ts
@@ -1,131 +1,280 @@
-import { Injectable } from '@angular/core';
-import { HttpClient} from '@angular/common/http';
-import { Global } from './global';
-import { Condicion } from '../models/condicion';
-import { map } from 'rxjs/operators';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Global } from "./global";
+import { Condicion } from "../models/condicion";
+import { map } from "rxjs/operators";
+import { reject } from "q";
 
 @Injectable()
 export class CondicionService {
+  public url: string;
+  public condicion: Array<any>;
+  constructor(private http: HttpClient) {
+    this.url = Global.url;
+  }
 
-    public url: string;
-    public condicion: Array<any>;
-    constructor(
-        private http: HttpClient,
-    ) {
-        this.url = Global.url;
-    }
+  getCamadas(): Promise<any> {
+    return this.http
+      .get(this.url + "/camada/", {
+        headers: { "Content-type": "application/json" }
+      })
+      .toPromise();
+  }
 
-    getCamadas(): Promise<any> {
-        return this.http.get(this.url + '/camada/', {headers: {'Content-type': 'application/json'}}).toPromise();
-    }
+  getChaveContas(): Promise<any> {
+    return this.http
+      .get(this.url + "/chavecontas/", {
+        headers: { "Content-type": "application/json" }
+      })
+      .toPromise();
+  }
 
-    getChaveContas(): Promise<any> {
-        return this.http.get(this.url + '/chavecontas/', {headers: {'Content-type': 'application/json'}}).toPromise();
-    }
+  getTiposValor(): Promise<any> {
+    return this.http
+      .get(this.url + "/tipovalor/", {
+        headers: { "Content-type": "application/json" }
+      })
+      .toPromise();
+  }
 
-    getTiposValor(): Promise<any> {
-        return this.http.get(this.url + '/tipovalor/', {headers: {'Content-type': 'application/json'}}).toPromise();
-    }
+  getSequenciasAcesso(): Promise<any> {
+    return this.http
+      .get(this.url + "/sequencia/", {
+        headers: { "Content-type": "application/json" }
+      })
+      .toPromise();
+  }
 
-    getSequenciasAcesso(): Promise<any> {
-        return this.http.get(this.url + '/sequencia/', {headers: {'Content-type': 'application/json'}}).toPromise();
-    }
+  getSequenciasByCondicao(): Promise<any> {
+    return this.http
+      .get(this.url + "/condicaosequencia/", {
+        headers: { "Content-type": "application/json" }
+      })
+      .toPromise();
+  }
 
-    getSequenciasByCondicao(): Promise<any> {
-        return this.http.get(this.url + '/condicaosequencia/', {headers: {'Content-type': 'application/json'}}).toPromise();
-    }
+  postCondicao(co: Condicion): Promise<any> {
+    return this.http
+      .post(
+        this.url + "/condicao/",
+        {
+          Cod_Condicao: co.sCodCondicion,
+          Desc_Condicao: co.sDesCondicion,
+          Escala_Qtde: co.bEscalaQtde ? 1 : 0,
+          POS_NEG: co.bNeg ? "N" : "P",
+          TIP_BASE_VENDAS: co.oCamada.TIPO_BASE_VENDAS,
+          MANDATORIA: 0,
+          ESTATISTICA: 0,
+          id_Camada: co.oCamada.id,
+          id_ChaveContas: co.oChaveContas.id,
+          id_TipoValor: co.oTipoValor.id
+        },
+        { headers: { "Content-type": "application/json" } }
+      )
+      .pipe(
+        map((elem: any) =>
+          co.aSequencias.forEach(campo => {
+            this.postCondicaoSequencia(elem.id, campo.id).subscribe();
+          })
+        )
+      )
+      .toPromise();
+  }
 
-    postCondicao(co: Condicion): Promise<any> {
-        return this.http.post(this.url + '/condicao/', {
-            Cod_Condicao: co.sCodCondicion,
-            Desc_Condicao: co.sDesCondicion,
-            Escala_Qtde: co.bEscalaQtde ? 1 : 0,
-            POS_NEG: co.bNeg ? 'N' : 'P',
-            TIP_BASE_VENDAS: co.oCamada.TIPO_BASE_VENDAS,
-            MANDATORIA: 0,
-            ESTATISTICA: 0,
-            id_Camada: co.oCamada.id,
-            id_ChaveContas: co.oChaveContas.id,
-            id_TipoValor: co.oTipoValor.id
-        }, {headers: {'Content-type': 'application/json'}})
-         .pipe(
-             map((elem: any) =>
-                co.aSequencias.forEach(campo => {
-                    this.postCondicaoSequencia(elem.id, campo.id).subscribe();
-                }))
-         ).toPromise();
-    }
+  editCondicao(co: Condicion): Promise<any> {
+    return this.http
+      .put(
+        this.url + "/condicao/" + co.sId + "/",
+        {
+          Cod_Condicao: co.sCodCondicion,
+          Desc_Condicao: co.sDesCondicion,
+          Escala_Qtde: co.bEscalaQtde ? 1 : 0,
+          POS_NEG: co.bNeg ? "N" : "P",
+          TIP_BASE_VENDAS: co.oCamada.TIPO_BASE_VENDAS,
+          MANDATORIA: 0,
+          ESTATISTICA: 0,
+          id_Camada: co.oCamada.id,
+          id_ChaveContas: co.oChaveContas.id,
+          id_TipoValor: co.oTipoValor.id
+        },
+        { headers: { "Content-type": "application/json" } }
+      )
+      .pipe(
+        map((elem: any) =>
+          co.aSequencias.forEach(campo => {
+            this.postCondicaoSequencia(elem.id, campo.id).subscribe();
+          })
+        )
+      )
+      .toPromise();
+  }
 
-    editCondicao(co: Condicion): Promise<any> {
-        return this.http.put(this.url + '/condicao/' + co.sId + '/', {
-            Cod_Condicao: co.sCodCondicion,
-            Desc_Condicao: co.sDesCondicion,
-            Escala_Qtde: co.bEscalaQtde ? 1 : 0,
-            POS_NEG: co.bNeg ? 'N' : 'P',
-            TIP_BASE_VENDAS: co.oCamada.TIPO_BASE_VENDAS,
-            MANDATORIA: 0,
-            ESTATISTICA: 0,
-            id_Camada: co.oCamada.id,
-            id_ChaveContas: co.oChaveContas.id,
-            id_TipoValor: co.oTipoValor.id
-        }, {headers: {'Content-type': 'application/json'}})
-         .pipe(
-             map((elem: any) =>
-                co.aSequencias.forEach(campo => {
-                    this.postCondicaoSequencia(elem.id, campo.id).subscribe();
-                }))
-         ).toPromise();
-    }
+  async deleteRelation(id: any) {
+    await this.http
+      .delete(this.url + "/condicaosequencia/" + id + "/", {
+        headers: { "Content-type": "application/json" }
+      })
+      .toPromise();
+  }
 
-    async deleteRelation(id: any) {
-        await this.http.delete(this.url + '/condicaosequencia/' + id + '/', {headers: {'Content-type': 'application/json'}}).toPromise();
-    }
+  getLastCondicao(): Promise<any> {
+    return this.http
+      .get(this.url + "/condicao/last/", {
+        headers: { "Content-type": "application/json" }
+      })
+      .toPromise();
+  }
 
-    getLastCondicao(): Promise<any> {
-        return this.http.get(this.url + '/condicao/last/', { headers: { 'Content-type': 'application/json' }}).toPromise();
-    }
+  deleteCondicaoSequencia(id: any) {
+    const arr: any = [];
+    const getSequenciasCondicao = this.http
+      .get(this.url + "/condicaosequencia/", {
+        headers: { "Content-type": "application/json" }
+      })
+      .toPromise();
+    getSequenciasCondicao.then((elems: any) => {
+      elems.map((sc: any) => {
+        if (sc.id_Condicao === id) {
+          arr.push(sc);
+        }
+      });
+      arr.map((sc: any) => {
+        this.deleteRelation(sc.id);
+      });
+    });
+  }
 
-    deleteCondicaoSequencia(id: any) {
-        const arr: any = [];
-        const getSequenciasCondicao = this.http.get(
-            this.url + '/condicaosequencia/', {headers: {'Content-type': 'application/json'}}).toPromise();
-        getSequenciasCondicao.then(
-            (elems: any) => {
-                elems.map(
-                    (sc: any) => {
-                        if (sc.id_Condicao === id) {
-                            arr.push(sc);
-                        }
-                    });
-                arr.map((sc: any) => {
-                    this.deleteRelation(sc.id);
-                });
-            });
-    }
+  postCondicaoSequencia(condId, seqId) {
+    return this.http.post(
+      this.url + "/condicaosequencia/",
+      {
+        id_Condicao: condId,
+        id_Sequencia: seqId
+      },
+      { headers: { "Content-type": "application/json" } }
+    );
+  }
 
-    postCondicaoSequencia(condId, seqId) {
-        return this.http.post(this.url + '/condicaosequencia/', {
-            id_Condicao: condId,
-            id_Sequencia: seqId
-        }, {headers: {'Content-type': 'application/json'}});
-    }
+  public getCondicaoByCode(): Promise<any> {
+    return this.http
+      .get(this.url + "/condicao/", {
+        headers: { "Content-type": "application/json" }
+      })
+      .toPromise();
+  }
 
-    public getCondicaoByCode(): Promise<any> {
-        return this.http.get(this.url + '/condicao/', {headers: {'Content-type': 'application/json'}}).toPromise();
-    }
+  public getCondicaos(): Promise<any> {
+    return this.http
+      .get(this.url + "/condicao/", {
+        headers: { "Content-type": "application/json" }
+      })
+      .toPromise();
+  }
 
-    public getCondicaos(): Promise<any> {
-        return this.http.get(this.url + '/condicao/', {headers: {'Content-type': 'application/json'}}).toPromise();
-    }
+  public getCondicaoCamada(): Promise<any> {
+    return this.http
+      .get(this.url + "/condicaocamada/", {
+        headers: { "Content-type": "application/json" }
+      })
+      .toPromise();
+  }
 
-    public getCondicaoCamada(): Promise<any> {
-        return this.http.get(this.url + '/condicaocamada/', {headers: {'Content-type': 'application/json'}}).toPromise();
-    }
+  public getCamadaEsquema(): Promise<any> {
+    return this.http
+      .get(this.url + "/camadaesquema/", {
+        headers: { "Content-type": "application/json" }
+      })
+      .toPromise();
+  }
 
-    public getCamadaEsquema(): Promise<any> {
-        return this.http.get(this.url + '/camadaesquema/', {headers: {'Content-type': 'application/json'}}).toPromise();
-    }
+  /*
+      Andrés Atencio 14/03/2020
+      Get condicao by camada schema
+      based on base vendas type
+    */
+  public getCondicaoCamadaByCamadaEsquema(baseVendas: string): Promise<any> {
+
+    const fetchData = () => {
+      const camadas = this.getCamadas();
+      const camadaEsquemas = this.getCamadaEsquema();
+      const condicaoCamadas = this.getCondicaoCamada();
+      const condicaos = this.getCondicaoByCode();
+      return [camadas, camadaEsquemas, condicaoCamadas, condicaos];
+    };
+
+    const filterCamadaByBaseVendas = baseType => e =>
+      e.TIPO_BASE_VENDAS === baseType;
+
+    const promise = new Promise((resolve, reject) => {
+
+      Promise.all(fetchData()).then(
+        ([camadas, camadaEsquemas, condicaoCamadas, condicaos]) => {
+          const camadasFiltered = camadas.filter(
+            filterCamadaByBaseVendas(baseVendas)
+          );
+
+          const camadasFullData = camadasFiltered.map(camada => {
+            const filterCondicaoCamadaByCamadaEsquema = (
+              camadaEsquemas,
+              condicaoCamadas
+            ) => {
+              return camadaEsquemas.map(camadaEsquema => {
+                return condicaoCamadas.filter(
+                  condCamada =>
+                    condCamada.id == camadaEsquema.id_Condicao_Camada
+                )[0];
+              });
+            };
+            const condicaoCamadasFiltered = filterCondicaoCamadaByCamadaEsquema(
+              camadaEsquemas,
+              condicaoCamadas
+            );
+
+            const filterCondicaosByCondicaoCamada = (
+              condicaoCamadasFilter,
+              condicaos
+            ) => {
+              return condicaoCamadasFilter.map(condCamada => {
+                return condicaos.filter(
+                  cond => cond.id == condCamada.id_Condicao
+                )[0];
+              });
+            };
+
+            const condicaosFiltered = filterCondicaosByCondicaoCamada(
+              condicaoCamadasFiltered,
+              condicaos
+            );
+
+            let condicaosByCamadaFilter = condicaosFiltered.filter(
+              cond => cond.id_Camada == camada.id
+            );
+
+            const condicaosAllow = (camada, condicaos) => {
+              const condicaosCamadasFiltered = condicaoCamadas.filter(
+                condCam => condCam.id_Camada == camada.id
+              );
+              return condicaosCamadasFiltered.map(condCamada => {
+                return condicaos.filter(
+                  cond => cond.id == condCamada.id_Condicao
+                )[0];
+              });
+            };
+
+            const condicaosAllowed = condicaosAllow(camada, condicaos);
+
+            return {
+              camada,
+              condicaosAllow: condicaosAllowed,
+              condicaos: condicaosByCamadaFilter
+            };
+          });
+
+          resolve(camadasFullData)
+        }
+      ).catch(err => reject(`Error getCondicaoCamadaByCamadaEsquema(${baseVendas})`));
+    });
+
+    return promise;
+  }
 }
-
-
-
