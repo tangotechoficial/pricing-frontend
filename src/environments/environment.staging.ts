e4b195949ff1a27777ae7d1d27cb4e56e6682716
Isaque Alves <isaquealves@gmail.com>
2020-03-23 18:35:07 -0300

    Setup da url correta da api em staging

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: true,
-  apiUrl: 'http://backend2.tangotechapp.com:8000/api'
+  production: false,
+  apiUrl: 'http://backend2.tangotechapp.com:8001/api'
 };
