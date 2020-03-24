8b7c5773398babf661d62566a97faa62417667d9
Isaque Alves <isaquealves@gmail.com>
2020-03-24 11:10:24 -0300

    Cria estrutura da tela execução

--- src/app/components/execucao/execucao.component.ts
+++ src/app/components/execucao/execucao.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { PurchasePlanningService } from '@services/purchasePlanning.service'
+import { first } from 'rxjs/operators'
+import { PurchasePlan } from '@models/purchaseplan'
+declare var $: any;
 
 @Component({
   selector: 'app-execucao',
   templateUrl: './execucao.component.html',
-  styleUrls: ['./execucao.component.scss']
+  styleUrls: ['./execucao.component.scss'],
+  providers: [ PurchasePlanningService],
 })
 export class ExecucaoComponent implements OnInit {
+  public planningData: Array<any> = new Array<any>();
 
-  constructor() { }
+  constructor(
+    private planningDataService: PurchasePlanningService
+  ) { }
 
   ngOnInit() {
+
+    this.planningDataService.planningData.pipe(first()).subscribe(
+          data => data.default.map(
+            row => {
+              this.planningData.push(new PurchasePlan().deserialize(row))
+            }
+          ), // should change this in real world
+          err => console.log(err)
+        )
   }
 
+
+
+  filter() {
+    $('#modalFilter').modal('show')
+  }
 }
