9f3e75048abe804e7a57dd592b209b9401f549fd
Iván Lynch <ivan.lynch@outlook.com.ar>
2020-04-01 16:00:41 -0300

    URL changed

--- src/app/services/global.ts
+++ src/app/services/global.ts
@@ -1,4 +1,4 @@
 export let Global = {
     url: 'http://backend2.tangotechapp.com:8000/api',
-    login: 'https://pricing.tangotechapp.com/api'
+    login: 'http://backend2.tangotechapp.com:8000/api'
 };
