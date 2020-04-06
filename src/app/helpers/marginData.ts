a7d6cc3d643db0e9e3c3e9a9d6aa7eec634c7427
Isaque Alves <isaquealves@gmail.com>
2020-04-06 04:58:12 -0300

    Add data helpers

---
+++ src/app/helpers/marginData.ts
@@ -0,0 +1,42 @@
+export var marginData = [
+  [
+    {
+      name: "margem sugerida",
+      value: 0.79
+    },
+    {
+      name: "margem planejada",
+      value: 0.83
+    }
+  ],
+  [
+    {
+      name: "margem sugerida",
+      value: 0.29
+    },
+    {
+      name: "margem planejada",
+      value: 0.83
+    }
+  ],
+  [
+    {
+      name: "margem sugerida",
+      value: 0.79
+    },
+    {
+      name: "margem planejada",
+      value: 0.33
+    }
+  ],
+  [
+    {
+      name: "margem sugerida",
+      value: 1.79
+    },
+    {
+      name: "margem planejada",
+      value: 2.83
+    }
+  ]
+]
