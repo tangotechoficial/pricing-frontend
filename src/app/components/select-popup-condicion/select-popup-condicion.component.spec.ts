58a529b16d59fe1e52c08eb375de32694b5dc97b
Charlie Minnelli <cminnelli@gmail.com>
2020-04-11 12:20:36 -0300

    seteo de tests@

--- src/app/components/select-popup-condicion/select-popup-condicion.component.spec.ts
+++ src/app/components/select-popup-condicion/select-popup-condicion.component.spec.ts
@@ -1,5 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Condicao } from 'app/models/condicao';
 import { SelectPopupCondicionComponent } from './select-popup-condicion.component';
 
 describe('SelectPopupCondicionComponent', () => {
@@ -20,6 +20,31 @@ describe('SelectPopupCondicionComponent', () => {
   });
 
   it('should create', () => {
-    // expect(component).toBeTruthy();
+    expect(component).toBeTruthy();
   });
+
+  it('getSelectedCondicao', () => {
+    let testCond = new Condicao();
+    component.getSelectedCondicao(testCond, 1)
+    expect(component.currenObject).toEqual(testCond);
+    expect(component.selectedItem).toBeTruthy();
+  })
+
+
+  it('event closePopup', () => {
+    // spy on event emitter
+    const component = fixture.componentInstance;
+    spyOn(component.closePopup, 'emit');
+
+    // trigger the click
+    const nativeElement = fixture.nativeElement;
+    const button = nativeElement.querySelector('button');
+    button.dispatchEvent(new Event('click'));
+
+    fixture.detectChanges();
+
+    expect(component.closePopup.emit).toHaveBeenCalledWith(false);
+
+  })
+
 });
