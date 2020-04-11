6d6b2e812991fcfde1c5bb453b534b5cdc3d7ebf
Isaque Alves <isaquealves@gmail.com>
2020-04-11 17:51:37 -0300

    Diretriz estrategica major project

--- src/app/components/diretriz-estrategica/diretriz-estrategica.component.ts
+++ src/app/components/diretriz-estrategica/diretriz-estrategica.component.ts
@@ -1,8 +1,12 @@
-import { Component, OnInit, ɵConsole } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { FilterModalService } from '@services/filtermodal.service';
+import { Component, OnInit, OnChanges, ChangeDetectorRef, SimpleChanges } from '@angular/core';
+import {FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { DiretrizesEstrategicasService } from '../../services/diretrizes-estrategicas.service';
 import { TechnicalMenuComponent } from '../navegacion/technical-menu/technical-menu.component';
 import { timingSafeEqual } from 'crypto';
-import {} from '@models/directory';
+import { Diretrix } from '@models/diretrix';
+import { Filter } from '@models/filter';
 
 @Component({
   selector: 'app-diretriz-estrategica',
@@ -10,7 +14,10 @@ import {} from '@models/directory';
   styleUrls: ['./diretriz-estrategica.component.scss'],
   providers: [ DiretrizesEstrategicasService ]
 })
-export class DiretrizEstrategicaComponent implements OnInit {
+export class DiretrizEstrategicaComponent implements OnInit, OnChanges {
+
+  filterForm: FormGroup;
+  filter: Filter = new Filter();
 
   public diretrizesEstrategicas: Array<any>;
   public directories: any;
@@ -19,6 +26,7 @@ export class DiretrizEstrategicaComponent implements OnInit {
   public subCategories: any;
   public fornecedores: any;
   public filial: any;
+
   public sumVLRVNDFATLIQ = 0;
   public sumVLRMRGCRB = 0;
   public sumVLRMRGBRT = 0;
@@ -26,34 +34,46 @@ export class DiretrizEstrategicaComponent implements OnInit {
   public MRGCRB = 0;
   public MRGBRT = 0;
 
-  // public Margem Contribuição Martins:
-
-
 
   constructor(
     private diretrixService: DiretrizesEstrategicasService,
+    private formBuilder: FormBuilder,
+    private filterService: FilterModalService,
+    private spinner: NgxSpinnerService,
+    private cdr: ChangeDetectorRef
   ) { }
 
   ngOnInit() {
     this.diretrizesEstrategicas = new Array<any>();
+    this.filterForm = this.formBuilder.group({
+      codfilfat: [Validators.required],
+      codfilepd: [Validators.required],
+      codestuni: [Validators.required],
+      codprd: [Validators.required]
+    })
     Promise.all([
       this.diretrixService.diretrizesEstrategicas.then(
-      data => data.map(
-        row => {
-          this.diretrizesEstrategicas.push(row);
-          this.sumVLRVNDFATLIQ = (Number(this.sumVLRVNDFATLIQ) + Number(row.VLRVNDFATLIQ));
-          this.sumVLRMRGCRB = (Number(this.sumVLRMRGCRB) + Number(row.VLRMRGCRB));
-          this.sumVLRMRGBRT = (Number(this.sumVLRMRGBRT) + Number(row.VLRMRGBRT));
-          this.sumVLRRCTLIQAPU = (Number(this.sumVLRRCTLIQAPU) + Number(row.VLRRCTLIQAPU));
-          this.MRGCRB =  (Number(this.sumVLRMRGCRB)  / Number(this.sumVLRRCTLIQAPU)) * 100;
-          this.MRGBRT =  (Number(this.sumVLRMRGBRT) / Number(this.sumVLRRCTLIQAPU)) * 100;
-        }
+        data => data.map(
+          row => {
+            this.diretrizesEstrategicas.push(row);
+            this.sumVLRVNDFATLIQ = (Number(this.sumVLRVNDFATLIQ) + Number(row.VLRVNDFATLIQ));
+            this.sumVLRMRGCRB = (Number(this.sumVLRMRGCRB) + Number(row.VLRMRGCRB));
+            this.sumVLRMRGBRT = (Number(this.sumVLRMRGBRT) + Number(row.VLRMRGBRT));
+            this.sumVLRRCTLIQAPU = (Number(this.sumVLRRCTLIQAPU) + Number(row.VLRRCTLIQAPU));
+            this.MRGCRB =  (Number(this.sumVLRMRGCRB)  / Number(this.sumVLRRCTLIQAPU)) * 100;
+            this.MRGBRT =  (Number(this.sumVLRMRGBRT) / Number(this.sumVLRRCTLIQAPU)) * 100;
+          }
+        )
       )
-    )
-    .catch(error => console.error(error)),
-    this.diretrixService.getDirectories().then(directories => {this.directories = directories;})
-  ]);
+      .catch(error => console.error(error)),
+      this.diretrixService.getDirectories().then(directories => {this.directories = directories;})
+    ]);
   }
+
+  ngOnChanges(changes: SimpleChanges) {
+
+  }
+
 
   loadGroups(value){
     return this.diretrixService.getGroups(value.currentTarget.value).then( groups => { this.groups = groups; } );
