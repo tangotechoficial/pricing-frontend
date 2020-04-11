b5630bf72f19a646b5a8203e8a8a72de03d377ac
Isaque Alves <isaquealves@gmail.com>
2020-04-11 15:53:28 -0300

    Add method to get categories from db

--- src/app/services/diretrizes-estrategicas.service.ts
+++ src/app/services/diretrizes-estrategicas.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '@env/environment';
 import { Diretrix } from '@models/diretrix';
 import { Directory } from '@app/models/directory';
+import { Category } from '@models/category'
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +13,7 @@ export class DiretrizesEstrategicasService {
 
   public url = `${environment.apiUrl}/pricing_parsing/diretrizesestrategicas`;
   public directoryURL = `${environment.apiUrl}/pricing_parsing/diretrizesdirectories`;
-
+  public categoryUrl = `${environments.apiUrl}/pricing_parsing/diretrizescategories/`
   constructor(private http: HttpClient) { }
 
   public get diretrizesEstrategicas(): Promise<Diretrix[]> {
@@ -32,7 +33,20 @@ export class DiretrizesEstrategicasService {
       (response: any) => {
         return response.results as Directory[];
       }
-    ).catch(error => {throw new Error(error)});
+    ).catch(error => { throw new Error(error); });
+  }
+
+  getCategories(param: number): Promise<Category[]> {
+    const options = param ?
+   { params: new HttpParams().set('CODGRPMER', param) } : {};
+    return this
+      .http
+      .get(this.categoryUrl, options)
+      .toPromise()
+      .then((response: any) => {
+        return response.results as Category[];
+      })
+      .catch(error => { throw new Error(error ); });
 
   }
 
