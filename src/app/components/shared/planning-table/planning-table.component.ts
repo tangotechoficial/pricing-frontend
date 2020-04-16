59005b8a9c1e94c54d7ca6e2e67006ff8821e421
Isaque Alves <isaquealves@gmail.com>
2020-04-16 14:12:40 -0300

    Apply form on planning table

--- src/app/components/shared/planning-table/planning-table.component.ts
+++ src/app/components/shared/planning-table/planning-table.component.ts
@@ -1,3 +1,5 @@
+import { PlanningDataManagerService } from '@app/services/planning-data.service';
+import { Group } from '@app/models/group';
 import { Input, Component, AfterViewInit } from '@angular/core';
 import {FormGroup, FormControl, Validators, FormArray, FormBuilder } from '@angular/forms';
 import { PlannedData } from '@models/plannedData';
@@ -16,28 +18,36 @@ export class PlanningTableComponent implements AfterViewInit {
   _data: any;
   @Input() hasRealized;
   fullForm: FormGroup;
-  formArray: FormArray;
   plannedData: PlannedData[];
 
 
   constructor(
     private formBuilder: FormBuilder,
-    private popupService: PlanningPopupDataManagerService
+    private popupService: PlanningPopupDataManagerService,
+    private planningDataManager: PlanningDataManagerService
     ) {
       this.plannedData = []
-    // this.fullForm = this.formBuilder.group()
+      this.fullForm = new FormGroup({});
+      this._data = this.planningDataManager.currentPlanValue;
+      this.planningDataManager.actualPlanData.subscribe(planningData => this._data = planningData)
+  }
+
+  buildGroups() {
+    debugger;
+    const weeksGroup = new FormGroup({});
+    this._data.map( (row, index) => {
+      const str = row.NUMANOMESSMN;
+      row.week = str.slice(-1);
+      row.year = str.slice(0, 4);
+      row.month = str.slice(4, 6);
+      weeksGroup.addControl('week' + index, this.buildFormGroup(row));
+    });
+    this.fullForm.addControl('weeks', weeksGroup);
   }
 
   @Input() set data(data: any) {
     if (data) {
-      this._data = data;
-      this._data.map( row => {
-        const str = row.NUMANOMESSMN;
-        row.week = str.slice(-1);
-        row.year = str.slice(0, 4);
-        row.month = str.slice(4, 6);
-
-      });
+      this.buildGroups();
     }
   }
 
@@ -47,51 +57,39 @@ export class PlanningTableComponent implements AfterViewInit {
     return this._data;
   }
 
-  private buildFormGroups() {
-    if (!this._data === undefined) {
-      // this._data.map( resultRow => {
-      //   const formGroup = this.formBuilder.group({
-      //     CODPRD: [''],
-      //     CODFILEPD: [''],
-      //     CODFILFAT: [''],
-      //     CODESTUNI: [''],
-      //     NUMANOMESSMN: [''],
-      //     VLRVNDLIQCAL: [''],
-      //     VLRVNDLIQOCD: [''],
-      //     VLRMCDOCD: [''],
-      //     VLRPCOMEDMCD: [''],
-      //     MRGBRTPEROCD: [''],
-      //     VLRPCOVNDLIQOCD: [''],
-      //     VLRPCOBSEOCD: [''],
-      //     VLRIMPTOTOCD: [''],
-      //     VLRICMOCD: [''],
-      //     VLRPISOCD: [''],
-      //     VLRDVLOCD: [''],
-      //     VLRFLXPLN: [''],
-      //     VLRMRGBRTOCD: [''],
-      //     VLRRBTCAL: [''],
-      //     VLRVBAOCD: [''],
-      //     VLRCMVCAL: [''],
-      //     VLRCMVPCOATU: [''],
-      //     VLRCSTCMPIDL: ['']
-      //   });
-
-      //   this.fullForm.push(formGroup)
-
-      // });
-      return;
-    }
+  private buildFormGroup(data) {
+    return this.formBuilder.group({
+      ID: [data.id],
+      CODPRD: [data.CODPRD],
+      CODFILEPD: [data.CODFILEPD],
+      CODFILFAT: [data.CODFILFAT],
+      CODESTUNI: [data.CODESTUNI],
+      NUMANOMESSMN: [data.NUMANOMESMN],
+      VLRVNDLIQCAL: [Number(data.VLRVNDLIQCAL).toFixed(2)],
+      VLRVNDLIQOCD: [Number(data.VLRVNDLIQOCD).toFixed(2)],
+      VLRMCDOCD: [Number(data.VLRMCDOCD).toFixed(2)],
+      VLRPCOMEDMCD: [Number(data.VLRPCOMEDMCD).toFixed(2)],
+      VLRPCOVNDLIQOCD: [Number(data.VLRPCOVNDLIQOCD).toFixed(2)],
+      VLRPCOBSEOCD: [Number(data.VLRPCOBSEOCD).toFixed(2)],
+      VLRIMPTOTOCD: [Number(data.VLRIMPTOTOCD).toFixed(2)],
+      VLRICMOCD: [Number(data.VLRICMOCD).toFixed(2)],
+      VLRPISOCD: [Number(data.VLRPISOCD).toFixed(2)],
+      VLRDVLOCD: [Number(data.VLRDVLOCD).toFixed(2)],
+      VLRFLXPLN: [Number(data.VLRFLXPLN).toFixed(2)],
+      VLRMRGBRTOCD: [Number(data.VLRMRGBRTOCD).toFixed(2)],
+      VLRRBTCAL: [Number(data.VLRRBTCAL).toFixed(2)],
+      VLRVBAOCD: [Number(data.VLRVBAOCD).toFixed(2)],
+      VLRCMVPCOATU: [Number(data.VLRCMVPCOATU).toFixed(2)],
+      VLRCSTCMPIDL: [Number(data.VLRCSTCMPIDL).toFixed(2)]
+    });
 
   }
-
   ngAfterViewInit() {
-    this.buildFormGroups();
     this.popupService.actualPlanData.subscribe(data => this.plannedData.push(data));
+    this.planningDataManager.actualPlanData.subscribe(planningData => this._data = planningData)
+    this.buildGroups()
   }
 
-  captureChanges(evt) {
-    return;
-  }
 
   triggerModalFor($event) {
     $('#planningPopup').show();
