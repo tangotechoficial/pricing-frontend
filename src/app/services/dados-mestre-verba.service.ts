36965d3e19a48520e533029b196e73fb4ea25daa
Isaque Alves <isaquealves@gmail.com>
2020-04-17 10:09:35 -0300

    Activate infinite scroll in pages

--- src/app/services/dados-mestre-verba.service.ts
+++ src/app/services/dados-mestre-verba.service.ts
@@ -1,26 +1,66 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 import { environment } from '@env/environment';
 import { MasterDataMoney } from '@models/masterdatamoney'
 @Injectable({
   providedIn: 'root'
 })
 export class DadosMestreVerbaService {
-  
+
   public dadosMestreVerbaUrl = `${environment.apiUrl}/pricing_parsing/dadosmestreverba`;
+  public nextUrlSubject: BehaviorSubject<string>;
+  public nextUrlObservable: Observable<string>;
+  public previousUrlSubject: BehaviorSubject<string>;
+  public previousUrlObservable: Observable<string>;
 
-  constructor(private http: HttpClient) { }
-
-  listar() {
-    return this.http.get<any[]>(this.dadosMestreVerbaUrl)
+  constructor(private http: HttpClient) {
+    this.nextUrlSubject = new BehaviorSubject<string>('');
+    this.nextUrlObservable = this.nextUrlSubject.asObservable();
+    this.previousUrlSubject = new BehaviorSubject<string>('');
+    this.previousUrlObservable = this.previousUrlSubject.asObservable();
   }
 
-  public get dadosMestresVerba(): Promise<MasterDataMoney[]>{
+  listar() {
+    return this.http.get<any[]>(this.dadosMestreVerbaUrl);
+  }
+
+  get nextUrlValue() {
+    return this.nextUrlSubject.value;
+  }
+
+  get previousUrlValue() {
+    return this.previousUrlSubject.value;
+  }
+
+  public get dadosMestresVerba(): Promise<MasterDataMoney[]> {
     const result = this.http.get(this.dadosMestreVerbaUrl);
     return result.toPromise()
-    .then((result: any) => {
-      return result.results as MasterDataMoney[];
+    .then((response: any) => {
+      if ( response.next ) {
+        this.nextUrlSubject.next(response.next);
+      }
+      return response.results as MasterDataMoney[];
+    })
+    .catch(err => {
+      throw new Error(err);
+    });
+  }
+
+  public loadNext(url): Promise<MasterDataMoney[]> {
+    if (!url) {
+      return;
+    }
+    const result = this.http.get(url);
+    return result.toPromise()
+    .then((response: any) => {
+      if ( response.next ) {
+        this.nextUrlSubject.next(response.next);
+      }
+      if ( response.previous ) {
+        this.previousUrlSubject.next(response.previous);
+      }
+      return response.results as MasterDataMoney[];
     })
     .catch(err => {
       throw new Error(err);
--- src/app/services/dados-mestres-composicao-preco.service.ts
+++ src/app/services/dados-mestres-composicao-preco.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 import { environment } from '@env/environment';
 import {PriceComposition} from '@models/pricecomposition'
+
 
 @Injectable({
   providedIn: 'root'
@@ -10,18 +11,61 @@ import {PriceComposition} from '@models/pricecomposition'
 export class DadosMestresComposicaoPrecoService {
 
   public dadosMestresPrecoUrl = `${environment.apiUrl}/pricing_parsing/dadosmestrecomposicao`;
+  public nextUrlSubject: BehaviorSubject<string>;
+  public nextUrlObservable: Observable<string>;
+  public previousUrlSubject: BehaviorSubject<string>;
+  public previousUrlObservable: Observable<string>;
 
-  constructor (private http: HttpClient) { }
+
+  constructor(private http: HttpClient) {
+    this.nextUrlSubject = new BehaviorSubject<string>('');
+    this.nextUrlObservable = this.nextUrlSubject.asObservable();
+    this.previousUrlSubject = new BehaviorSubject<string>('');
+    this.previousUrlObservable = this.previousUrlSubject.asObservable();
+   }
 
   listar() {
-    return this.http.get<any[]>(this.dadosMestresPrecoUrl)
+    return this.http.get<any[]>(this.dadosMestresPrecoUrl);
   }
 
-  public get dadosMestresPreco(): Promise<PriceComposition[]>{
+  get nextUrlValue() {
+    return this.nextUrlSubject.value;
+  }
+
+  get previousUrlValue() {
+    return this.previousUrlSubject.value;
+  }
+
+  public get dadosMestresPreco(): Promise<PriceComposition[]> {
     const result = this.http.get(this.dadosMestresPrecoUrl);
     return result.toPromise()
-    .then((result: any) => {
-      return result.results as PriceComposition[];
+    .then((response: any) => {
+
+      if ( response.next ) {
+        this.nextUrlSubject.next(response.next);
+      }
+      return response.results as PriceComposition[];
+    })
+    .catch(err => {
+      throw new Error(err);
+    });
+  }
+
+  public loadNext(url): Promise<PriceComposition[]> {
+    if(!url) {
+      return;
+    }
+    const result = this.http.get(url);
+    return result.toPromise()
+    .then((response: any) => {
+
+      if ( response.next ) {
+        this.nextUrlSubject.next(response.next);
+      }
+      if ( response.previous ) {
+        this.previousUrlSubject.next(response.previous);
+      }
+      return response.results as PriceComposition[];
     })
     .catch(err => {
       throw new Error(err);
