face3e64961ec85b7d0b08f93b3f3994d86f4803
Isaque Alves <isaquealves@gmail.com>
2020-04-17 07:18:59 -0300

    Fix chart issues

--- src/app/components/shared/selling-composition-charts/selling-composition-charts.component.ts
+++ src/app/components/shared/selling-composition-charts/selling-composition-charts.component.ts
@@ -15,32 +15,47 @@ import * as pluginDataLabels from 'chartjs-plugin-datalabels';
 export class SellingCompositionChartsComponent {
 
   sellingData = sellingData;
-  public chartOptions: ChartOptions = {
+  public options: ChartOptions = {
     responsive: true,
     legend: {
-      position: 'right',
+      position: 'right'
     },
     plugins: {
       datalabels: {
         formatter: (value, ctx) => {
-          const label = ctx.chart.data.labels[ctx.dataIndex];
-          return label;
+          return Math.round(value * 100) / 100;
         },
-      },
+        backgroundColor: 'white',
+        borderColor: 'white',
+        borderRadius: 45,
+        borderWidth: 1,
+        color: 'black',
+        font: {
+          weight: 'bold'
+        },
+        display: (context) => {
+          return context.dataset.data[context.dataIndex] !== 0; // or >= 1 or ...
+       }
+      }
     },
     events: [
-      'click'
+      'click',
+      'hover'
     ]
   };
 
   public ariaLabels: string[] = [];
   public volumeCompositionType: ChartType = 'pie';
   public volumeCompositionChartDataSugerido: ChartDataSets[] = [];
-  public sugeridoLabels: Label[] = ['Impostos', 'Devolução', 'Margem Bruta Unitária', 'Verba Sugerida', 'Verba Planejada', 'CMV Sugerido'];
+  public sugeridoLabels: Label[] = [
+    'Impostos', 'Devolução', 'Margem Bruta Unitária',
+    'Verba Sugerida', 'Verba Planejada', 'CMV Sugerido'];
   public volumeCompositionChartDataPlanejado: ChartDataSets[] = [];
-  public planejadoLabels: Label[] = ['Impostos', 'Devolução', 'Margem Bruta Unitária', 'Verba Sugerida', 'Verba Planejada', 'CMV Planejado'];
+  public planejadoLabels: Label[] = [
+    'Impostos', 'Devolução', 'Margem Bruta Unitária',
+    'Verba Sugerida', 'Verba Planejada', 'CMV Planejado'];
   public plugins = [pluginDataLabels];
-  public chartLegend = true
+  public chartLegend = true;
 
   planningData: PurchasePlan[];
 
@@ -50,12 +65,12 @@ export class SellingCompositionChartsComponent {
   updateChartsData() {
       this.planningData.forEach((item, index) => {
         this.ariaLabels.push(`nav-week${index + 1}`);
-        const impostos = Number(item.VLRIMPTOTCAL).toFixed(2);
-        const devolucao = Number(item.VLRDVLCAL).toFixed(2);
-        const margemBrutaUnitaria = Number(item.VLRMRGBRTCAL).toFixed(2);
-        const verbaSugerida = Number(item.VLRRBTOCD).toFixed(2);
-        const verbaPlanejada = Number(item.VLRVBAOCD).toFixed(2);
-        const cmvPrecoSugerido = Number(item.VLRCMVCAL).toFixed(2);
+        const impostos = Number(Number(item.VLRIMPTOTCAL).toFixed(2));
+        const devolucao = Number(Number(item.VLRDVLCAL).toFixed(2));
+        const margemBrutaUnitaria = Number(Number(item.VLRMRGBRTCAL).toFixed(2));
+        const verbaSugerida = Number(Number(item.VLRRBTOCD).toFixed(2));
+        const verbaPlanejada = Number(Number(item.VLRVBAOCD).toFixed(2));
+        const cmvPrecoSugerido = Number(Number(item.VLRCMVCAL).toFixed(2));
         this.volumeCompositionChartDataSugerido[index] = {
           data: [],
           label: 'Sugerido',
@@ -71,12 +86,12 @@ export class SellingCompositionChartsComponent {
           label: 'Planejado',
           backgroundColor: ['#6289CF', '#FF6F50', '#E6DE4C', '#E66C43', '#79F09F', '#4270C1']
         };
-        const impostos = Number(item.VLRIMPTOTOCD).toFixed(2);
-        const devolucao = Number(item.VLRDVLOCD).toFixed(2);
-        const margemBrutaUnitaria = Number(item.VLRMRGBRTOCD).toFixed(2);
-        const verbaSugerida = Number(item.VLRRBTCAL).toFixed(2);
-        const verbaPlanejada = Number(item.VLRVBAOCD).toFixed(2);
-        const cmvPrecoSugerido = Number(item.VLRCMVOCD).toFixed(2);
+        const impostos = Number(Number(item.VLRIMPTOTOCD).toFixed(2));
+        const devolucao = Number(Number(item.VLRDVLOCD).toFixed(2));
+        const margemBrutaUnitaria = Number(Number(item.VLRMRGBRTOCD).toFixed(2));
+        const verbaSugerida = Number(Number(item.VLRRBTCAL).toFixed(2));
+        const verbaPlanejada = Number(Number(item.VLRVBAOCD).toFixed(2));
+        const cmvPrecoSugerido = Number(Number(item.VLRCMVOCD).toFixed(2));
         this.volumeCompositionChartDataPlanejado[index].data.push(
             impostos, devolucao, margemBrutaUnitaria, verbaSugerida, verbaPlanejada, cmvPrecoSugerido);
       });
